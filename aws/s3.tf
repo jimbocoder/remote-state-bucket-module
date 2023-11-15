7cab2c5892a481a471b54e29f79931a8315fef0a
ItamarMalka <itamar.malka@env0.com>
2023-11-15 11:38:38 +0200

    add state encryption

--- aws/s3.tf
+++ aws/s3.tf
@@ -10,6 +10,16 @@ resource "aws_s3_bucket_versioning" "state_versioning" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "state_public_access_block" {
   bucket = aws_s3_bucket.state_bucket.id
 
